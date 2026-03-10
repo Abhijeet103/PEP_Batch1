40bb6c678f56a4ca01c52c0875a4daa47c149c07
Abhijeet <abhijeet@spotter.ai>
2026-03-10 20:35:58 +0530

    controller basics

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,60 +1,39 @@
 package com.example.demo.controller;
 
 
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class UserController {
 
-   @Autowired
-    UserService userService;
-
-    @GetMapping("/home")
-    String fun(){
-       return """
-               <!DOCTYPE html>
-               <html lang="en">
-               <head>
-                   <meta charset="UTF-8">
-                   <title>My First Webpage</title>
-               </head>
-               <body>
-                   <h1>Welcome to My Website</h1>
-                   <p>This is my first paragraph of text.</p>
-                   <a href="https://www.example.com">Visit an Example Website</a>
-               </body>
-               </html>
-             
-               """ ;
-    }
 
     @GetMapping("/user")
-    void getUser(@RequestParam String email){
-
-        System.out.println(email);
-//        return userService.findAll()  ;
+    String fun(@RequestParam String name , @RequestParam boolean isAdmin){
+        if(isAdmin){
+            return "Hello Admin " + name;
+        }
+       return "Hello User  " + name;
     }
 
     @GetMapping("/user/{id}")
-    User getOneUser(@PathVariable Integer id) {
-        return userService.findById(id);
+    String fun1(@PathVariable int id){
+
+        return "Hello User " + id ;
     }
 
-
     @PostMapping("/user")
-    UserRequest createrUser(@RequestBody UserRequest userRequest){
-        System.out.println(userRequest.getEmail());
-        System.out.println(userRequest.getPassword());
-        System.out.println(userRequest.getName());
+    ResponseEntity<UserRequest> fun2(@RequestBody UserRequest userRequest  , @RequestHeader("Authorization") String token){
+        try {
+            System.out.println(userRequest);
+            userRequest.setName("Abhijeet");
+            return new ResponseEntity<>(userRequest, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
-        return userRequest ;
     }
 
 
--- src/main/java/com/example/demo/controller/UserRequest.java
+++ src/main/java/com/example/demo/controller/UserRequest.java
@@ -4,12 +4,14 @@ package com.example.demo.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequest {
+
+    String email;
     String name;
     String password;
-    String email ;
 }
