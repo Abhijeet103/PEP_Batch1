fc1ca4f8d7e58d7439e4fc51c1f51cf78cc83f49
Abhijeet <abhijeet@spotter.ai>
2026-03-11 19:52:56 +0530

    docker basics

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Integer> {
 
-
+    @Query("select p from Post p where p.id = :id")
+    List<Post> customFuntion(long id);
 
 }
